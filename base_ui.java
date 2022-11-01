47849dc56444a5ecb4adcff665d75230027475cb
Sriparno Ganguly <k2e7@outlook.com>
2022-11-01 11:42:52 +0530

    plotEllipse done

--- base_ui.java
+++ base_ui.java
@@ -36,7 +36,7 @@ class WorkBench extends JPanel
 
     // plots a point with a settable color.
     public void plotpoint(int x, int y, Graphics G) {
-        G.fillRect(originX + x * delta - delta / 8, originY - y * delta - delta / 8, delta / 4, delta / 4);
+        G.fillRect(originX + x * delta - delta / 2, originY - y * delta - delta / 2, delta , delta );
         // G.fillOval(originX+x*delta-delta/8,originY-y*delta-delta/8,delta/4,delta/4);
     }
 
@@ -109,6 +109,52 @@ class WorkBench extends JPanel
             G.drawString("(0,0)", originX + delta / 2, originY + delta / 2);
     }
 
+    // plots an Ellipse using Midpoint Ellipse Drawing Algorithm.
+    public void plotEllipse(int a, int b, int xc, int yc, Graphics G) {
+        double d2;
+
+        int x=0;
+        int y=b;
+
+        double d1 = b*b - a*a*b + 0.25*a*a;
+        plotpoint((int)(x + xc),(int)(y + yc),G);
+        plotpoint((int)(-x + xc),(int)(y + yc),G);
+        plotpoint((int)(x + xc),(int)(-y + yc),G);
+        plotpoint((int)(-x + xc),(int)(-y + yc),G);
+
+        while(a*a*(y-0.5) > b*b*(x+1)) {
+            if(d1<0) {
+                d1 += b*b*(2*x+3);
+            }
+            else {
+                d1 += b*b*(2*x+3) + a*a*(-2*y+2);
+                y--;
+            }
+            x++;
+            plotpoint((int)(x + xc),(int)(y + yc),G);
+            plotpoint((int)(-x + xc),(int)(y + yc),G);
+            plotpoint((int)(x + xc),(int)(-y + yc),G);
+            plotpoint((int)(-x + xc),(int)(-y + yc),G);
+        } // Region 1 //
+
+        d2 = b*b*(x+0.5)*(x+0.5) + a*a*(y-1)*(y-1) - a*a*b*b;
+        while(y>0) {
+            if (d2<0) {
+                d2 += b*b*(2*x+2) + a*a*(-2*y+3);
+                x++;
+            }
+            else
+                d2 += a*a*(-2*y+3);
+            y--;
+            plotpoint((int)(x + xc),(int)(y + yc),G);
+            plotpoint((int)(-x + xc),(int)(y + yc),G);
+            plotpoint((int)(x + xc),(int)(-y + yc),G);
+            plotpoint((int)(-x + xc),(int)(-y + yc),G);
+        } // Region 2 //
+
+    }
+
+    //public void setEllipse()
     protected void paintComponent(Graphics G) {
         super.paintComponent(G);
         // int x, y, x2, y2;
@@ -133,6 +179,8 @@ class WorkBench extends JPanel
 
         G.setColor(Color.green);
         plotpoint(originX + 1, originY + 1, G);
+
+        plotEllipse(15, 15, 0, 0, G);
     }
 
     // driving the zooming using Mouse Wheel movement
@@ -149,16 +197,14 @@ class WorkBench extends JPanel
     }
 
     public void actionPerformed(ActionEvent ae) {
-        if(ae.getActionCommand() == "Zoomed In")
-        {
+        if (ae.getActionCommand() == "Zoomed In") {
             delta = delta + 5;
             delta = delta > 150 ? 150 : delta;
         }
 
-        if(ae.getActionCommand() == "Zoomed Out")
-        {
+        if (ae.getActionCommand() == "Zoomed Out") {
             delta = delta - 5;
-            delta = delta < 5 ? 5 : delta;
+            delta = delta < 1 ? 1 : delta;
         }
         repaint();
     }
