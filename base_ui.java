f2d6ef0283b5ff981375a3ba81c3b11326bfb2d5
Sriparno Ganguly <k2e7@outlook.com>
2022-11-01 12:57:38 +0530

    adding Rotational support

--- base_ui.java
+++ base_ui.java
@@ -136,8 +136,8 @@ class WorkBench extends JPanel
 
         std.plotEllipse(15, 15, 0, 0, G);
         std.plotLine(0, 0, 15, 15, G);
-        //std.plotTriangle(3, -4, -3, -4, 0, 5, G);
-        //std.plotQuad(6, 6, 6, -6, -6, -6, -6, 6, G);
+        // std.plotTriangle(3, -4, -3, -4, 0, 5, G);
+        // std.plotQuad(6, 6, 6, -6, -6, -6, -6, 6, G);
     }
 
     // driving the zooming using Mouse Wheel movement
@@ -238,6 +238,43 @@ class StdDrawing extends WorkBench {
         }
     } // MidPoint Line //
 
+}
+
+class RotationalTransforms {
+    int ox, oy, angle;
+
+    public RotationalTransforms(int ox,int oy,int angle)
+        {
+            this.ox=ox;
+            this.oy=oy;
+            this.angle=angle;
+        }
+
+    public int rotateX(int x, int y) {
+        double angle = Math.PI / 180 * this.angle;
+        double px = x * Math.cos(angle) - y * Math.sin(angle) + ox * (1.0 - Math.cos(angle)) + oy * Math.sin(angle);
+        return (int) px;
+    }
+
+    public int rotateY(int x, int y) {
+        double angle = Math.PI / 180 * this.angle;
+        double py = x * Math.sin(angle) + y * Math.cos(angle) + oy * (1.0 - Math.cos(angle)) - ox * Math.sin(angle);
+
+        return (int) py;
+    }
+
+    public boolean setAngle(int angle) {
+        if (angle < 0 || angle > 360)
+            return false;
+        this.angle = angle;
+        return true;
+    }
+
+    public boolean setPivot(int pX, int pY) {
+        this.ox = pX;
+        this.oy = pY;
+        return true;
+    }
 }
 
 // Class where the UI is organized and managed.
