59978abb62e03060c4ac8aa130d0279858ea582d
Sriparno Ganguly <k2e7@outlook.com>
2022-11-03 22:35:22 +0530

    editing

--- base_ui.java
+++ base_ui.java
@@ -25,8 +25,8 @@ class WorkBench extends JPanel
         addMouseWheelListener(this);
         zoomIN = new JButton("Zoom In");
         zoomOUT = new JButton("Zoom Out");
-        zoomIN.setBounds(0,0,20,20);
-        zoomOUT.setBounds(0,0,20,20);
+        zoomIN.setBounds(0, 0, 20, 20);
+        zoomOUT.setBounds(0, 0, 20, 20);
         add(zoomIN);
         add(zoomOUT);
         zoomIN.addActionListener(this);
@@ -95,7 +95,7 @@ class WorkBench extends JPanel
     protected void paintComponent(Graphics G) {
         super.paintComponent(G);
         StdDrawing std = new StdDrawing();
-        OtherParts op  = new OtherParts();
+        OtherParts op = new OtherParts();
         height = getHeight();
         width = getWidth();
 
@@ -116,31 +116,31 @@ class WorkBench extends JPanel
         int y1 = 10;
         int radius = 60;
         RotationalTransforms rt = new RotationalTransforms(x1, y1 + radius, 35);
-        new Body(x1, y1, radius/2, radius, SpotType.SPOTTED, HairType.HAIRLESS, rt, G);
-        op.head(radius/2, x1, y1+3* radius/2, 4, 2, G);
-        
-        int leg1px = rt.rotateX(x1, y1-radius);
-        int leg1py = rt.rotateY(x1, y1-radius);
-        int leg2px = rt.rotateX(x1-5, y1-radius+10);
-        int leg2py = rt.rotateY(x1-5, y1-radius+10);
+        new Body(x1, y1, radius / 2, radius, SpotType.SPOTTED, HairType.HAIRLESS, rt, G);
+        op.head(radius / 2, x1, y1 + 3 * radius / 2, 4, 2, G);
+
+        int leg1px = rt.rotateX(x1, y1 - radius);
+        int leg1py = rt.rotateY(x1, y1 - radius);
+        int leg2px = rt.rotateX(x1 - 5, y1 - radius + 10);
+        int leg2py = rt.rotateY(x1 - 5, y1 - radius + 10);
 
         G.setColor(Color.yellow);
-        new MovableParts(leg1px, leg1py, radius/3, 3*radius/2, SpotType.SPOTTED, HairType.HAIRLESS, 0, 5, G);
-        new MovableParts(leg2px, leg2py, radius/3, 3*radius/2, SpotType.SPOTTED, HairType.HAIRLESS, -20, 5, G);
-    
-        int hand1px = rt.rotateX(x1-radius/2+10, y1+radius/2);
-        int hand1py = rt.rotateY(x1-radius/2+10, y1+radius/2);
-        int hand2px = rt.rotateX(x1-radius/2+10, y1+radius/2+10);
-        int hand2py = rt.rotateY(x1-radius/2+10, y1+radius/2+10);
+        new MovableParts(leg1px, leg1py, radius / 3, 3 * radius / 2, SpotType.SPOTTED, HairType.HAIRLESS, 0, 5, G);
+        new MovableParts(leg2px, leg2py, radius / 3, 3 * radius / 2, SpotType.SPOTTED, HairType.HAIRLESS, -20, 5, G);
 
-        new MovableParts(hand1px, hand1py, radius/4, radius, SpotType.SPOTTED, HairType.HAIRLESS, -45, -90, G);
-        new MovableParts(hand2px, hand2py, radius/4, radius, SpotType.SPOTTED, HairType.HAIRLESS, -75, -120, G);
+        int hand1px = rt.rotateX(x1 - radius / 2 + 10, y1 + radius / 2);
+        int hand1py = rt.rotateY(x1 - radius / 2 + 10, y1 + radius / 2);
+        int hand2px = rt.rotateX(x1 - radius / 2 + 10, y1 + radius / 2 + 10);
+        int hand2py = rt.rotateY(x1 - radius / 2 + 10, y1 + radius / 2 + 10);
 
-        int tailX = rt.rotateX(x1+17, y1-radius);
-        int tailY = rt.rotateY(x1+17, y1-radius);
+        new MovableParts(hand1px, hand1py, radius / 4, radius, SpotType.SPOTTED, HairType.HAIRLESS, -45, -90, G);
+        new MovableParts(hand2px, hand2py, radius / 4, radius, SpotType.SPOTTED, HairType.HAIRLESS, -75, -120, G);
+
+        int tailX = rt.rotateX(x1 + 17, y1 - radius);
+        int tailY = rt.rotateY(x1 + 17, y1 - radius);
         rt.setPivot(tailX, tailY);
         rt.setAngle(-10);
-        new Body(tailX, tailY, radius/3, radius/8, SpotType.SPOTLESS, HairType.HAIRY, rt, G);
+        new Body(tailX, tailY, radius / 3, radius / 8, SpotType.SPOTLESS, HairType.HAIRY, rt, G);
     }
 
     // driving the zooming using Mouse Wheel movement
